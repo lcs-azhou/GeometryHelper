04faf2606dfcfb7c5ecb8bec5b445147ce52c4e3
Ansheng Zhou <azhou@lcs.on.ca>
2023-11-16 09:06:59 -0500

    In our view, we are now making use of the existing Circle structure from our model.

--- GeometryHelper/Views/CircleView.swift
+++ GeometryHelper/Views/CircleView.swift
@@ -10,14 +10,14 @@ import SwiftUI
 struct CircleView: View {
     
 //    mark:stored properties
-    @State var radius = 10.0
+    @State var currentCircle = Circle(radius: 10.0)
     
 //    mark:computed properties
     var body: some View {
         VStack {
-            Text("\(radius)")
+            Text("\(currentCircle.radius)")
                 .font(.largeTitle)
-            Slider(value: $radius, in: 0...100)
+            Slider(value: $currentCircle.radius, in: 0...100)
         }.padding()
         
     }
