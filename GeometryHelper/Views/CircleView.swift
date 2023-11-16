1e1ea10ea79660e79322c50718bfe8bc40c51cf8
Ansheng Zhou <azhou@lcs.on.ca>
2023-11-16 11:35:45 -0500

    finish the Stack

--- GeometryHelper/Views/CircleView.swift
+++ GeometryHelper/Views/CircleView.swift
@@ -14,15 +14,60 @@ struct CircleView: View {
     
 //    mark:computed properties
     var body: some View {
+        
+        
+        
         VStack {
-            Text("\(currentCircle.radius)")
-                .font(.largeTitle)
+            Image("formula")
+                .resizable()
+                .scaledToFit()
+            HStack {
+                Text("0")
+                    .font(.system(size: 18))
+                
+                Slider(value: $currentCircle.radius, in: 0...100)
+                
+                Text("100")
+                    .font(.system(size: 18))
+            }.padding()
+        }
+        
+        List {
+            VStack(alignment: .leading){
+                Text("Radius")
+                    .fontWeight(.semibold)
+                Text("\(currentCircle.radius)")
+                    .fontWeight(.light)
+                    .font(.system(size: 15))
+            }.frame(height: 55)
             
-            Slider(value: $currentCircle.radius, in: 0...100)
+            VStack(alignment: .leading){
+                Text("Diameter")
+                    .fontWeight(.semibold)
+                Text("\(currentCircle.diameter)")
+                    .fontWeight(.light)
+                    .font(.system(size: 15))
+            }.frame(height: 55)
             
-            Text("\(currentCircle.diameter)")
-                .font(.largeTitle)
-        }.padding()
+            VStack(alignment: .leading){
+                Text("Perimeter")
+                    .fontWeight(.semibold)
+                Text("\(currentCircle.perimeter)")
+                    .fontWeight(.light)
+                    .font(.system(size: 15))
+            }.frame(height: 55)
+            
+            VStack(alignment: .leading){
+                Text("Area")
+                    .fontWeight(.semibold)
+                Text("(square units)")
+                    .font(.system(size: 12))
+                Text("\(currentCircle.area)")
+                    .fontWeight(.light)
+                    .font(.system(size: 15))
+            }.frame(height: 55)
+            
+        }.listStyle(.plain)
         
     }
     
